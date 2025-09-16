74adb9517d12fff56c53108d73861cbe0ff07a60
Vega Feng <vega.feng@oocl.com>
2025-09-16 11:46:47 +0800

    fix(day12): fix the annoation for controller

--- src/main/java/com/example/day12todolist/controller/ToDoController.java
+++ src/main/java/com/example/day12todolist/controller/ToDoController.java
@@ -2,11 +2,14 @@ package com.example.day12todolist.controller;
 
 import com.example.day12todolist.service.ToDoService;
 import org.hibernate.annotations.ConcreteProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * @author FENGVE
  */
-@ConcreteProxy
+@Controller
 public class ToDoController {
+    @Autowired
     private ToDoService toDoService;
 }
