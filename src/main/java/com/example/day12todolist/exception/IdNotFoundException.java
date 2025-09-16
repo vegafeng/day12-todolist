9343c675d2b1c35a08c05764530a2e2636ca97e0
Vega Feng <vega.feng@oocl.com>
2025-09-16 16:46:10 +0800

    feat(day12): add exception for id not found

---
+++ src/main/java/com/example/day12todolist/exception/IdNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.day12todolist.exception;
+
+import java.security.InvalidKeyException;
+
+/**
+ * @author FENGVE
+ */
+public class IdNotFoundException extends InvalidKeyException {
+}
--- src/main/java/com/example/day12todolist/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/day12todolist/exception/handler/GlobalExceptionHandler.java
@@ -4,7 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.security.InvalidKeyException;
 
 /**
  * @author FENGVE
@@ -14,6 +17,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public String illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        return e.getMessage();
+    }
+    @ExceptionHandler(InvalidKeyException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String invalidKeyException(InvalidKeyException e) {
         return e.getMessage();
     }
 
