a5224dbd4f60afa6cd713303b2784c9c67e4bd6b
Vega Feng <vega.feng@oocl.com>
2025-09-16 14:55:20 +0800

    refactor(day12): change method for CORS

---
+++ src/main/java/com/example/day12todolist/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.day12todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author FENGVE
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
